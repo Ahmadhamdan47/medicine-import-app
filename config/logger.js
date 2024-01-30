ceb452bc2d058f5b21bce1c506c477bd146b1ca8
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-01-30 20:38:31 +0200

    comments

--- config/logger.js
+++ config/logger.js
@@ -1,16 +1,27 @@
-// ./config/logger.js
-
+// Import the winston library, a logging library for Node.js
 const winston = require('winston');
 
+// Create a new logger instance with two transports: Console and File
 const logger = winston.createLogger({
+  // The transports option specifies where the logs should be outputted
   transports: [
+    // The Console transport outputs the logs to the console
     new winston.transports.Console(),
+    
+    // The File transport outputs the logs to a file
+    // In this case, the file is named 'logfile.log'
     new winston.transports.File({ filename: 'logfile.log' }),
   ],
+  
+  // The format option specifies how the logs should be formatted
   format: winston.format.combine(
+    // The timestamp format prepends a timestamp to the logs
     winston.format.timestamp(),
+    
+    // The simple format outputs the logs in a simple, readable format
     winston.format.simple()
   ),
 });
 
+// Export the logger instance to be used in other parts of the application
 module.exports = logger;
--- src/controllers/drugController.js
+++ src/controllers/drugController.js
@@ -1,57 +1,75 @@
-// src/controllers/drugController.js
-
-const DrugService = require('../services/drugService');
-
+// Define an asynchronous function named 'searchDrugByATCName'
 const searchDrugByATCName = async (req, res) => {
+  // Extract the 'query' parameter from the request
   const query = req.params.query;
 
   try {
+    // Call the 'searchDrugByATCName' function from the DrugService module, passing the 'query' as an argument
     const result = await DrugService.searchDrugByATCName(query);
+    
+    // Send the result in the response
     res.json(result);
   } catch (error) {
+    // If an error occurs, send a 500 status code and an error message in the response
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
+// Define an asynchronous function named 'searchDrugByBrandName'
 const searchDrugByBrandName = async (req, res) => {
+  // Extract the 'query' parameter from the request
   const query = req.params.query;
 
   try {
+    // Call the 'searchDrugByBrandName' function from the DrugService module, passing the 'query' as an argument
     const result = await DrugService.searchDrugByBrandName(query);
+    
+    // Send the result in the response
     res.json(result);
   } catch (error) {
+    // If an error occurs, send a 500 status code and an error message in the response
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
+// Define an asynchronous function named 'getDrugByGuid'
 const getDrugByGuid = async (req, res) => {
   try {
+    // Extract the 'guid' parameter from the request
     const { guid } = req.params;
-    const drug = await getDrugByGuid(guid);
+    
+    // Call the 'getDrugByGuid' function from the DrugService module, passing the 'guid' as an argument
+    const drug = await DrugService.getDrugByGuid(guid);
+    
+    // If the drug is not found, send a 404 status code and an error message in the response
     if (!drug) {
       return res.status(404).json({ error: 'Drug not found' });
     }
+    
+    // Send the drug in the response
     res.json(drug);
   } catch (error) {
+    // If an error occurs, send a 500 status code and the error message in the response
     res.status(500).json({ error: error.toString() });
   }
 };
 
+// Define an asynchronous function named 'filterDrugs'
 const filterDrugs = async (req, res) => {
+  // Extract the 'query' parameter from the request
   const query = req.params.query;
 
   try {
+    // Call the 'searchDrugByATCName' function from the DrugService module, passing the 'query' as an argument
     const searchResults = await DrugService.searchDrugByATCName(query);
+    
+    // Call the 'filterDrugs' function from the DrugService module, passing the search results as an argument
     const filteredResults = await DrugService.filterDrugs(searchResults);
+    
+    // Send the filtered results in the response
     res.json(filteredResults);
   } catch (error) {
+    // If an error occurs, send a 500 status code and an error message in the response
     res.status(500).json({ error: 'Internal Server Error' });
   }
-};
-
-module.exports = {
-  searchDrugByATCName,
-  searchDrugByBrandName,
-  getDrugByGuid,
-  filterDrugs,
 };
