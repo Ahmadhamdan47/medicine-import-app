1889cd5cb51b92061d3bd4abd032fad946c56b13
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-01-30 20:38:31 +0200

    comments

--- config/database.js
+++ config/database.js
@@ -1,14 +1,27 @@
+// Import the Sequelize library
 const Sequelize = require('sequelize');
 
+// Create a new Sequelize instance and connect to the 'MedLebDrugs' database
+// using the username 'sa' and the password 'Theroadof1'
 const sequelize = new Sequelize('MedLebDrugs', 'sa', 'Theroadof1', {
+  // The host of the database server
   host: 'localhost',
+  
+  // The dialect/engine of the database
   dialect: 'mssql',
+  
+  // Additional options for the dialect
   dialectOptions: {
+    // The name of the SQL Server instance
     instanceName: 'SQLEXPRESS',
   },
+  
+  // Global model options
   define: {
+    // Disable automatic timestamp fields (createdAt and updatedAt)
     timestamps: false,
   },
 });
 
+// Export the Sequelize instance to be used in other parts of the application
 module.exports = sequelize;
--- config/swagger.js
+++ config/swagger.js
@@ -1,20 +1,36 @@
 // config/swagger.js
 
+// Export the Swagger configuration
 module.exports = {
+    // The swaggerDefinition object contains information about the API
     swaggerDefinition: {
+      // The OpenAPI version
       openapi: '3.0.0',
+      
+      // Information about the API
       info: {
+        // The title of the API
         title: 'Medicine Import API',
+        
+        // The version of the API
         version: '1.0.0',
+        
+        // A description of the API
         description: 'API documentation for Medicine Import App',
       },
+      
+      // An array of servers where the API is hosted
       servers: [
         {
+          // The URL of the server
           url: 'http://localhost:3000',
+          
+          // A description of the server
           description: 'Local Development Server',
         },
       ],
     },
+    
+    // An array of paths to the API route files
     apis: ['./src/routes/*.js'],
   };
-  
