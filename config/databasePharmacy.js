c0d724f918a28cac8e2e63250b95b79d078861eb
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-03-20 09:20:16 +0200

    Database Connection

--- config/databasePharmacy.js
+++ config/databasePharmacy.js
@@ -1,24 +1,16 @@
+// config/databasePharmacyService.js
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('MedLebPharmacyServices', 'sa', 'Theroadof1', {
-    // The host of the database server
-    host: 'localhost',
-    
-    // The dialect/engine of the database
-    dialect: 'mssql',
-    
-    // Additional options for the dialect
-    dialectOptions: {
-      // The name of the SQL Server instance
-      instanceName: 'SQLEXPRESS',
-    },
-    logging: console.log,
-    
-    // Global model options
-    define: {
-      // Disable automatic timestamp fields (createdAt and updatedAt)
-      timestamps: false,
-    },
-  });
-  
-  module.exports = sequelize;
+const sequelize = new Sequelize('PharmacyService', 'sa', 'Theroadof1', {
+        host: 'localhost',
+        dialect: 'mssql',
+        dialectOptions: {
+            instanceName: 'SQLEXPRESS',
+        },
+        logging: console.log,
+        define: {
+            timestamps: false,
+        },
+});
+
+module.exports = sequelize;
