4746f858e8ae269b2bc7c0c93e87680e6b76b6ee
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-05-03 17:37:26 +0300

    stratumfix

--- src/models/drugStratum.js
+++ src/models/drugStratum.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/databasePharmacy');
 const Drug = require('./drug');
-const StratumType = require('./StratumType');
+const StratumType = require('./stratumType');
 
 const DrugStratum = sequelize.define('DrugStratum', {
     DrugStratumId: {
