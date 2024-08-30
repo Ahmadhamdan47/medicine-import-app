938272b3331e72fe24317a81b4cbacc276eb44ef
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-08-30 16:37:21 +0300

    box service fix

--- src/services/boxService.js
+++ src/services/boxService.js
@@ -1,41 +1,48 @@
 const { Box } = require('../models/box'); // Adjust the path as needed
 
 const BoxService = {
-  async addBox(donationId, boxLabel) {
+  createBox: async (boxData) => {
     try {
-      const newBox = await Box.create({ DonationId: donationId, BoxLabel: boxLabel });
+      const newBox = await Box.create({
+        DonationId: boxData.DonationId,
+        BoxLabel: boxData.BoxLabel,
+      });
       return newBox;
     } catch (error) {
-      throw new Error(`Error adding box: ${error.message}`);
+      console.error('Error adding box:', error);
+      throw new Error('Error adding box');
     }
   },
 
-  async deleteBox(boxId) {
+  deleteBox: async (boxId) => {
     try {
       const deleted = await Box.destroy({ where: { BoxId: boxId } });
       return deleted;
     } catch (error) {
+      console.error('Error deleting box:', error);
       throw new Error(`Error deleting box: ${error.message}`);
     }
   },
 
-  async getBoxes(donationId) {
+  getBoxes: async (donationId) => {
     try {
       const boxes = await Box.findAll({ where: { DonationId: donationId } });
       return boxes;
     } catch (error) {
+      console.error('Error fetching boxes:', error);
       throw new Error(`Error fetching boxes: ${error.message}`);
     }
   },
 
-  async updateBox(boxId, updateData) {
+  updateBox: async (boxId, updateData) => {
     try {
       const [updated] = await Box.update(updateData, { where: { BoxId: boxId } });
       return updated;
     } catch (error) {
+      console.error('Error updating box:', error);
       throw new Error(`Error updating box: ${error.message}`);
     }
-  }
+  },
 };
 
 module.exports = BoxService;
