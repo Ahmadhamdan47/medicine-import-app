67b8cbf50c3ee5ab10251567f6fe829f0504749e
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-09-02 16:07:35 +0300

    Passing active status through getAllDonors

--- src/models/associations/associations.js
+++ src/models/associations/associations.js
@@ -3,6 +3,7 @@ const BatchLotTracking = require('../BatchLot');
 const Box = require('../box');
 const Donor = require('../donor');
 const Recipient = require('../recipient');
+const UserAccounts = require('../userAccounts');
 
 // Associations
 Donation.hasMany(Box, { foreignKey: 'DonationId' });
@@ -15,3 +16,7 @@ Donation.belongsTo(Donor, { foreignKey: 'DonorId' });
 Donation.belongsTo(Recipient, { foreignKey: 'RecipientId' });
 Donor.hasMany(Donation, { foreignKey: 'DonorId' });
 Recipient.hasMany(Donation, { foreignKey: 'RecipientId' });
+
+
+Donor.hasOne(UserAccounts, { foreignKey: 'DonorId' });
+UserAccounts.belongsTo(Donor, { foreignKey: 'DonorId' });
