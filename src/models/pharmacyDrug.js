5c48a8fb2f7fdbe25570bda2f18a98e8d31ec778
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-02-01 15:32:02 +0200

    add SEQ and LNDI to model

--- src/models/pharmacyDrug.js
+++ src/models/pharmacyDrug.js
@@ -183,7 +183,13 @@ const NewDrug = sequelize.define('drug', {
     },
     CurrencyForeign: {
         type: DataTypes.UUID
-    }
+    },
+    Seq:{
+        type: DataTypes.STRING(255)
+    },
+    PresentationLNDI:{
+        type: DataTypes.STRING(255)
+    },
 }, {
     tableName: 'drug',
     timestamps: false // Assuming there are no 'createdAt' and 'updatedAt' fields in the table
