09fe7ccfecffdd3ca28dcbc913a60038e41b3a4e
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-02-15 18:42:48 +0200

    images.js

--- src/data/images.js
+++ src/data/images.js
@@ -2,34 +2,13 @@ const { Op } = require('sequelize');
 const NewDrug = require('../models/pharmacyDrug'); // Adjust the path as needed
 
 async function updateImagePath() {
-    const BATCH_SIZE = 1000; // Define the batch size
-    let offset = 0;
-    let hasMoreRecords = true;
-
     try {
-        while (hasMoreRecords) {
-            const drugs = await NewDrug.findAll({
-                offset: offset,
-                limit: BATCH_SIZE,
-            });
+        console.log('Updating ImagesPath for all records...');
 
-            if (drugs.length > 0) {
-                await NewDrug.update(
-                    { ImagesPath: 'drug.jpg' },
-                    {
-                        where: {
-                            DrugID: {
-                                [Op.in]: drugs.map(drug => drug.DrugID),
-                            },
-                        },
-                    }
-                );
-
-                offset += BATCH_SIZE;
-            } else {
-                hasMoreRecords = false;
-            }
-        }
+        await NewDrug.update(
+            { ImagesPath: 'drug.jpg' }, // Set new value
+            { where: {} } // This applies the update to all rows
+        );
 
         console.log('All records have been updated successfully.');
     } catch (error) {
