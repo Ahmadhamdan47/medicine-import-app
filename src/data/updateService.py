2de84cfee2bacb58334f505a5d1d9a8b246b55ea
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-03-11 01:52:00 +0200

    path fix 2

--- src/data/updateService.py
+++ src/data/updateService.py
@@ -29,7 +29,7 @@ def get_db_connection():
         return None
 
 def main():
-    tsv_data = read_tsv('/march.tsv')
+    tsv_data = read_tsv('./march.tsv')
 
     conn = get_db_connection()
     if conn is None:
