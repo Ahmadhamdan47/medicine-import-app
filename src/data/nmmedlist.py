bd4fec3be6e8cc30af4d6c685a375309a1b47fa0
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-06-04 16:58:23 +0300

    update

--- src/data/nmmedlist.py
+++ src/data/nmmedlist.py
@@ -51,7 +51,7 @@ def truncate_values(row, column_lengths):
     return row
 
 def main():
-    tsv_data = read_tsv('./may.tsv')
+    tsv_data = read_tsv('./june.tsv')
 
     conn = get_db_connection()
     if conn is None:
--- src/data/updateMedist.py
+++ src/data/updateMedist.py
@@ -31,7 +31,7 @@ def get_db_connection():
         return None
 
 def main():
-    tsv_data = read_tsv('./may.tsv')
+    tsv_data = read_tsv('./june.tsv')
 
     conn = get_db_connection()
     if conn is None:
