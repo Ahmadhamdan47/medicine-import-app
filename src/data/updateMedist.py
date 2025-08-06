76b5f3c03221776f4efd6b35f4fc823ffb7acf16
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-08-06 13:42:12 +0300

    august 8

--- src/data/updateMedist.py
+++ src/data/updateMedist.py
@@ -31,7 +31,7 @@ def get_db_connection():
         return None
 
 def main():
-    tsv_data = read_tsv('./june13.tsv')
+    tsv_data = read_tsv('./August-5.tsv')
 
     conn = get_db_connection()
     if conn is None:
